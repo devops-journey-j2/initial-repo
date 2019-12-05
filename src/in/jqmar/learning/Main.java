0974fb114bd01cf682a5ad6e9ecf290e836429d7
jQmar2019 <jQmar2019@outlook.com>
2019-12-05 11:18:33 +0400

    Second commit

---
+++ src/in/jqmar/learning/Main.java
@@ -0,0 +1,10 @@
+package in.jqmar.learning;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Only for test");
+    }
+}
